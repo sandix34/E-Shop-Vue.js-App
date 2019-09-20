9a9204380d65049162d21277dc2eb396a04183a4
sandrine <sandrinezocca@gmail.com>
2019-09-20 14:45:05 +0200

    création du header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
+    <the-header></the-header>
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader'
 
 export default {
   name: 'app',
   components: {
-    
+    TheHeader
   }
 }
 </script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" >
+      <img src="../assets/logo.png" alt="logo" width="30" height="30"/>
+      Vue-Shop
+    </a>
+    <button class="navbar-toggler">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link">Boutique</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Admin</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+ 
+}
+</script>
+
+<style>
+
+</style>
