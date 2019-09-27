c60b192ee58ee1ca788553ca70e5466e72c4fa07
sandrine <sandrinezocca@gmail.com>
2019-09-27 15:14:58 +0200

    mise en place de la navigation à l'aide d'un composant dynamique

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <the-header></the-header>
-    <user></user>
-    <admin></admin>
+    <div class="d-flex flex-column w-100">
+      <component :is="page"></component>
+    </div>
   </div>
 </template>
 
 <script>
+import { eventBus } from './main';
 import TheHeader from './components/TheHeader';
 import User from './components/features/User/User';
-import Admin from './components/features//Admin/Admin'
+import Admin from './components/features//Admin/Admin';
 
 export default {
   name: 'app',
@@ -17,6 +19,16 @@ export default {
     TheHeader,
     User,
     Admin
+  },
+  data() {
+    return {
+      page: eventBus.page
+    }
+  },
+  created() {
+    eventBus.$on('update:page', (page) => {
+      this.page = page;
+    })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -49,7 +49,8 @@ export const eventBus = new Vue({
         price: 1800
       }
     ],
-    cart: []
+    cart: [],
+    page:'User'
   },
   methods: {
     addProductToCart(product) {
@@ -61,6 +62,10 @@ export const eventBus = new Vue({
     removeItemFromCart(item) {
       this.cart = this.cart.slice().filter( i => i.id !== item.id );
       this.$emit('update:cart', this.cart.slice());
+    },
+    changePage(page) {
+      this.page = page;
+      this.$emit('update:page', this.page);
     }
   }
 })
