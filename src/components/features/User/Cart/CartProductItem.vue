68b36d00a91e97f325214837749dabb51366357a
sandrine <sandrinezocca@gmail.com>
2019-10-01 15:39:57 +0200

    mise en place d'un filtre global pour afficher le prix

--- src/components/features/User/Cart/CartProductItem.vue
+++ src/components/features/User/Cart/CartProductItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex flex-row justify-content-between p-2" >
     <span style="width:130px;">{{ item.title }}</span>
-    <span><strong>{{ item.price }} €</strong></span>
+    <span><strong>{{ item.price | price }}</strong></span>
     <button class="close" @click="removeItemFromCart">
       <span>&times;</span>
     </button>
